b76adafff880dd81dcda44e97d93dfec2af3927b
António Faneca <me@afaneca.com>
2018-01-03 20:24:38 +0000

    V0.5 - Versão Inicial

---
+++ Trabalho/Trabalho/Controllers/PerfilController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+using Microsoft.AspNet.Identity.EntityFramework;
+using Trabalho.Models;
+
+namespace Trabalho.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class PerfilController : Controller
+    {
+        ApplicationDbContext context;
+
+        public PerfilController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Trabalho/Trabalho/Controllers/PescadoresController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Trabalho.Models;
+
+namespace Trabalho.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class PescadoresController : Controller
+    {
+        private TPContext db = new TPContext();
+
+        // GET: Pescadores
+        public ActionResult Index()
+        {
+            return View(db.Pescadors.ToList());
+        }
+
+        // GET: Pescadores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pescador pescador = db.Pescadors.Find(id);
+            if (pescador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pescador);
+        }
+
+        // GET: Pescadores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pescadores/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,MyProperty")] Pescador pescador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pescadors.Add(pescador);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pescador);
+        }
+
+        // GET: Pescadores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pescador pescador = db.Pescadors.Find(id);
+            if (pescador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pescador);
+        }
+
+        // POST: Pescadores/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,MyProperty")] Pescador pescador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pescador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pescador);
+        }
+
+        // GET: Pescadores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pescador pescador = db.Pescadors.Find(id);
+            if (pescador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pescador);
+        }
+
+        // POST: Pescadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pescador pescador = db.Pescadors.Find(id);
+            db.Pescadors.Remove(pescador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
